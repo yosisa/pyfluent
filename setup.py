f04a2d1b7e6b9d58283881e6bb569afad823bf39
Yoshihisa Tanaka <yt.hisa@gmail.com>
2012-02-22 19:34:49 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,24 +7,26 @@ readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 long_description = open(readme).read()
 
 classifiers = [
-    'Development Status :: 1 - Planning',
-    'Intended Audience :: Developers',
-    'Intended Audience :: System Administrators',
     'Programming Language :: Python',
     'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: System :: Logging',
     'Topic :: System :: Networking'
 ]
 
 setup(name=name,
       version=version,
+      author='Yoshihisa Tanaka',
+      author_email='yt.hisa@gmail.com',
+      license='MIT',
       description='A python client library for Fluentd',
       long_description=long_description,
       classifiers=classifiers,
       keywords=['logging', 'fluentd', 'json'],
-      author='Yoshihisa Tanaka',
-      author_email='yt.hisa@gmail.com',
-      license='MIT',
       install_requires=['msgpack-python>=0.1.12'],
-      packages=find_packages(exclude=['*.tests*'])
+      tests_require=['pytest', 'mock'],
+      packages=find_packages(exclude=['tests'])
 )
